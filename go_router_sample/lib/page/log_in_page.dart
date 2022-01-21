449c2101343d4c0b328f016ce6cab9dabf709968
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-21 14:40:49 +0900

    copy_files

---
+++ go_router_sample/lib/page/log_in_page.dart
@@ -0,0 +1,26 @@
+import 'package:go_router_sample/page/book_list_page.dart';
+import 'package:flutter/material.dart';
+
+class LogInPage extends StatelessWidget {
+  const LogInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ログインページ'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('ログイン'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => BookListPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
