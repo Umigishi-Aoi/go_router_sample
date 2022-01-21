4b22831b4d8242a4f1c3d6cc5a7bb40c9562ec59
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-01-21 14:35:20 +0900

    add base project

---
+++ base/lib/page/book_detail_page.dart
@@ -0,0 +1,29 @@
+import 'package:base/data/book.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailPage extends StatelessWidget {
+  final Book book;
+
+  const BookDetailPage({Key? key, required this.book}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('本の詳細ページ${book.id}'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            Text('タイトル：${book.title}'),
+            const SizedBox(
+              height: 16,
+            ),
+            Text('著者：${book.author}'),
+          ],
+        ));
+  }
+}
